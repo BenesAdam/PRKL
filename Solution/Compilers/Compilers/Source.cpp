f354b8b7d11887bdb0f6192318dccb6d219875e1
BenesAdam <adam.bilovice@seznam.cz>
2022-05-29 16:10:34 +0200

    Done

--- Solution/Compilers/Compilers/Source.cpp
+++ Solution/Compilers/Compilers/Source.cpp
@@ -27,39 +27,193 @@ int Interpr(const Uzel* u)
 #define druhy u->z.z.druhy
 #define treti u->z.z.treti
 #define ctvrty u->z.z.ctvrty
+
 	switch (u->Typ) {
-	case 0: Interpr(prvni); Interpr(druhy);
+	case 0:
+		Interpr(prvni); Interpr(druhy);
 		return 0;
-	case '=': return *prvni->z.Adresa = Interpr(druhy);
-	case P_DELEN: { int d = Interpr(druhy);
-		if (d == 0) { printf("\nDeleni nulou\n"); abort(); }
-		return *prvni->z.Adresa /= d; }
-	case PRINT: if (prvni->Typ != RETEZ)
-		printf("%i", Interpr(prvni));
-						else if (druhy)
-		printf(prvni->z.Retez, Interpr(druhy));
-						else printf(prvni->z.Retez);
+	case '=':
+		return *prvni->z.Adresa = Interpr(druhy);
+	case P_DELEN:
+	{
+		int d = Interpr(druhy);
+		if (d == 0)
+		{
+			printf("\nDeleni nulou\n"); abort();
+		}
+		return *prvni->z.Adresa /= d;
+	}
+	case P_OR:
+		return *prvni->z.Adresa |= Interpr(druhy);
+	case P_XOR:
+		return *prvni->z.Adresa ^= Interpr(druhy);
+	case P_AND:
+		return *prvni->z.Adresa &= Interpr(druhy);
+	case P_POSUNVPRAVO:
+		return *prvni->z.Adresa >>= Interpr(druhy);
+	case P_POSUNVLEVO:
+		return *prvni->z.Adresa <<= Interpr(druhy);
+	case P_ODECT:
+		return *prvni->z.Adresa -= Interpr(druhy);
+	case P_PRICT:
+		return *prvni->z.Adresa += Interpr(druhy);
+	case P_MODUL:
+	{
+		int d = Interpr(druhy);
+		if (d == 0)
+		{
+			printf("\nDeleni nulou\n"); abort();
+		}
+		return *prvni->z.Adresa %= d;
+	}
+	case P_NASOB:
+		return *prvni->z.Adresa *= Interpr(druhy);
+	case PRINT:
+		if (prvni->Typ != RETEZ)
+			printf("%i", Interpr(prvni));
+		else if (druhy)
+			printf(prvni->z.Retez, Interpr(druhy));
+		else
+			printf(prvni->z.Retez);
 		return 0;
-	case FOR: Interpr(prvni);
-		while (Interpr(druhy)) {
+	case SCAN:
+		scanf("%i", prvni->z.Adresa);
+		return 0;
+	case FOR:
+		Interpr(prvni);
+		while (Interpr(druhy))
+		{
 			Interpr(ctvrty);
 			Interpr(treti);
 		}
 		return 0;
-	case IF: if (Interpr(prvni)) Interpr(druhy);
-				 else Interpr(treti);
+	case WHILE:
+		while (Interpr(prvni))
+		{
+			Interpr(druhy);
+		}
 		return 0;
-	case INKREM: if (prvni) return ++ * prvni->z.Adresa;
-		return (*druhy->z.Adresa)++;
-	case '!': return !Interpr(prvni);
-	case '<': return Interpr(prvni) < Interpr(druhy);
-	case OR: return Interpr(prvni) || Interpr(druhy);
-	case '-': if (druhy) return Interpr(prvni) - Interpr(druhy);
+	case DO:
+		do
+		{
+			Interpr(prvni);
+		} while (Interpr(druhy));
+		return 0;
+	case IF:
+		if (Interpr(prvni))
+		{
+			Interpr(druhy);
+		}
+		else
+		{
+			Interpr(treti);
+		}
+		return 0;
+	case ELSE:
+		Interpr(prvni);
+		return 0;
+	case INKREM:
+		if (prvni)
+		{
+			return ++(*prvni->z.Adresa);
+		}
+		else
+		{
+			return (*druhy->z.Adresa)++;
+		}
+	case DEKREM:
+		if (prvni)
+		{
+			return --(*prvni->z.Adresa);
+		}
+		else
+		{
+			return (*druhy->z.Adresa)--;
+		}
+	case POSUNVPRAVO:
+		return Interpr(prvni) >> Interpr(druhy);
+	case POSUNVLEVO:
+		return Interpr(prvni) << Interpr(druhy);
+	case UPLUS:
+		return Interpr(prvni);
+	case UMINUS:
 		return -Interpr(prvni);
-	case '/': { int d = Interpr(druhy); if (d == 0) { printf("\nDeleni nulou\n"); abort(); }
-					return Interpr(prvni) / d; }
-	case CISLO: return u->z.Cislo;
-	case PROMENNA: return *u->z.Adresa;
+	case '!':
+		return !Interpr(prvni);
+	case '<':
+		return Interpr(prvni) < Interpr(druhy);
+	case '>':
+		return Interpr(prvni) > Interpr(druhy);
+	case OR:
+		return Interpr(prvni) || Interpr(druhy);
+	case AND:
+		return Interpr(prvni) && Interpr(druhy);	
+	case NENIROVNO:
+		return Interpr(prvni) != Interpr(druhy);
+	case ROVNO:
+		return Interpr(prvni) == Interpr(druhy);
+	case VETSIROVNO:
+		return Interpr(prvni) >= Interpr(druhy);
+	case MENSIROVNO:
+		return Interpr(prvni) <= Interpr(druhy);
+	case '+':
+		if (druhy)
+		{
+			return Interpr(prvni) + Interpr(druhy);
+		}
+		else
+		{
+			return Interpr(prvni);
+		}
+	case '-':
+		if (druhy)
+		{
+			return Interpr(prvni) - Interpr(druhy);
+		}
+		else
+		{
+			return -Interpr(prvni);
+		}
+	case '*':
+		return Interpr(prvni) * Interpr(druhy);
+	case '/':
+	{
+		int d = Interpr(druhy);
+		if (d == 0)
+		{
+			printf("\nDeleni nulou\n");
+			abort();
+		}
+		else
+		{
+			return Interpr(prvni) / d;
+		}
+	}
+	case '%':
+	{
+		int d = Interpr(druhy);
+		if (d == 0)
+		{
+			printf("\nDeleni nulou\n");
+			abort();
+		}
+		else
+		{
+			return Interpr(prvni) % d;
+		}
+	}
+	case '&':
+		return Interpr(prvni) & Interpr(druhy);
+	case '|':
+		return Interpr(prvni) | Interpr(druhy);
+	case '^':
+		return Interpr(prvni) ^ Interpr(druhy);
+	case CISLO: 
+		return u->z.Cislo;
+	case PROMENNA: 
+		return *u->z.Adresa;
+	case RETEZ:
+		return *u->z.Retez;
 	default: printf("\nNeznama operace: %i\n", u->Typ); abort();
 	}
 }
--- Solution/Compilers/Compilers/mikroC.cpp
+++ Solution/Compilers/Compilers/mikroC.cpp
@@ -1,31 +1,84 @@
 #include "FlexBison/mikroC.h"
+#include <cstdlib>
+#include <cstdio>
+#include <string>
+#include <map>
+
+struct cmp_str
+{
+	bool operator()(char const* a, char const* b) const
+	{
+		return std::strcmp(a, b) < 0;
+	}
+};
+
+std::map<char*, Uzel*, cmp_str> variables;
 
 Uzel* GenUzel(int type, Uzel* first, Uzel* second, Uzel* third, Uzel* fourth)
 {
-	return nullptr;
+	Uzel* uzel = new Uzel;
+	uzel->Typ = type;
+	uzel->z.z.prvni = first;
+	uzel->z.z.druhy = second;
+	uzel->z.z.treti = third;
+	uzel->z.z.ctvrty = fourth;
+
+	return uzel;
 }
 
 Uzel* GenCislo(int number)
 {
-	return nullptr;
+	Uzel* uzel = new Uzel;
+	uzel->Typ = CISLO;
+	uzel->z.Cislo = number;
+
+	return uzel;
+}
+
+char* GetStr(const char* arg_str)
+{
+	unsigned int len = strlen(arg_str);
+	char* str = new char[len + 1];
+	str[len] = '\0';
+	strcpy(str, arg_str);
+	return str;
 }
 
 Uzel* GenRetez(const char* str)
 {
-	return nullptr;
+	Uzel* uzel = new Uzel;
+	uzel->Typ = RETEZ;
+	uzel->z.Retez = GetStr(str);
+
+	return uzel;
 }
 
 Uzel* GenPromen(const char* nameOfVariable)
 {
-	return nullptr;
+	Uzel* result;
+	char* key = GetStr(nameOfVariable);
+	if (variables.count(key) == 0)
+	{
+		Uzel* newVariable = new Uzel;
+		newVariable->Typ = PROMENNA;
+		newVariable->z.Adresa = new int;
+		variables.emplace(key, newVariable);
+		result = newVariable;
+	}
+	else
+	{
+		result = variables[key];
+		delete key;
+	}
+	return result;
 }
 
 void Chyba(const char* errorMessage, Pozice position)
 {
-
+	printf("%s\n", errorMessage);
 }
 
 void yyerror(const char* message)
 {
-
+	printf("%s\n", message);
 }
